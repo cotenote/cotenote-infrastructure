615030565dcfbfaa658567db14d5fe40816f070a
HelloMinchan <wjdalscksdle@naver.com>
2023-02-06 01:26:59 +0900

    feat: add aws provider

---
+++ infrastructure/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+variable "availability_zones" {
+  description = "availability zones"
+  type = object({
+    a = string
+    b = string
+    c = string
+    d = string
+  })
+  default = {
+    a = "ap-northeast-2a"
+    b = "ap-northeast-2b"
+    c = "ap-northeast-2c"
+    d = "ap-northeast-2d"
+  }
+}
