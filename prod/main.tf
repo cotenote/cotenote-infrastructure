a6f550fde12dfa87a509e81ecc8f56b274cf0a99
HelloMinchan <wjdalscksdle@naver.com>
2023-02-06 02:28:56 +0900

    feat: add internet gateway

--- prod/main.tf
+++ prod/main.tf
@@ -11,3 +11,10 @@ module "subnet" {
   vpc_id             = module.vpc.id
   availability_zones = var.availability_zones
 }
+
+# ========== Internet Gateway ========== #
+module "internet_gateway" {
+  source       = "../modules/internet_gateway"
+  project_name = var.project_name
+  vpc_id       = module.vpc.id
+}
