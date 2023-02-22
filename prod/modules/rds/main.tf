b777f74efeed19bf0c1a24aefe9f77fb43fd90e0
HelloMinchan <wjdalscksdle@naver.com>
2023-02-23 03:56:15 +0900

    refactor: change rds subnet group to public and set collation

--- prod/modules/rds/main.tf
+++ prod/modules/rds/main.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "rds" {
   engine                = "mysql"
   engine_version        = "8.0.28"
-  parameter_group_name  = "default.mysql8.0"
+  parameter_group_name  = aws_db_parameter_group.rds_parameter_group.name
   instance_class        = "db.t2.micro"
   allocated_storage     = 20
   max_allocated_storage = 100
@@ -10,6 +10,8 @@ resource "aws_db_instance" "rds" {
   db_name    = var.rds_env.db_name
   username   = var.rds_env.username
   password   = var.rds_env.password
+
+  publicly_accessible = true
 
   skip_final_snapshot = true
 
@@ -22,6 +24,52 @@ resource "aws_db_instance" "rds" {
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "${var.stage}-${var.project_name}-rds-subnet-group"
-  subnet_ids = [var.subnet_id.private_a, var.subnet_id.private_b]
+  name = "${var.stage}-${var.project_name}-rds-subnet-group"
+  #   subnet_ids = [var.subnet_id.private_a, var.subnet_id.private_b]
+  subnet_ids = [var.subnet_id.public_a, var.subnet_id.public_b]
+}
+
+resource "aws_db_parameter_group" "rds_parameter_group" {
+  name   = "${var.stage}-${var.project_name}-rds-parameter-group"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_connection"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_filesystem"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_results"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "collation_connection"
+    value = "utf8mb4_unicode_ci"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_unicode_ci"
+  }
 }
--- prod/modules/security_group/main.tf
+++ prod/modules/security_group/main.tf
@@ -36,6 +36,13 @@ resource "aws_security_group" "rds_security_group" {
   vpc_id      = var.vpc_id
 
   ingress {
+    description = "Allow home MySQL inbound traffic"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.home_ip]
+  }
+  ingress {
     description = "Allow api ec2 MySQL inbound traffic"
     from_port   = 3306
     to_port     = 3306
@@ -43,6 +50,12 @@ resource "aws_security_group" "rds_security_group" {
     cidr_blocks = ["${data.aws_instance.api_ec2.public_ip}/32"]
   }
 
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.home_ip]
+  }
   egress {
     from_port   = 3306
     to_port     = 3306
